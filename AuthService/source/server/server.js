aefdb0b8efc0e92f249049883c124be68641668d
rubber-bird <marko.turchyn@gmail.com>
2021-07-28 22:57:22 -0400

    Completed server

---
+++ AuthService/source/server/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const logger = require('morgan')
+const api = require('../api/auth')
+
+const startServer = (options) => {
+  return new Promise((resolve, reject) => {
+    if (!options.port) {
+      let err = new Error('The server should be started on available port')
+      reject(err)
+    }
+
+    const app = express()
+    app.use(morgan('dev'))
+    // app.use()
+
+    api(app, options)
+
+    const server = app.listen(options.port, () = resolve(server))
+  })
+}
+
+module.exports = Object.assign(({}, {startServer}));
