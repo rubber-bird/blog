0bd316d7ec70411cfa78f8fb577cffa6ac268a87
rubber-bird <marko.turchyn@gmail.com>
2021-07-29 23:12:05 -0400

    Bug fix

--- AuthService/source/index.js
+++ AuthService/source/index.js
@@ -1,2 +1,10 @@
+'use strict'
 
-console.log('Starting Auth Sevice...')
+const server = require('./server/server');
+const config = require('./config/index');
+
+console.log('-----------------------');
+console.log('Starting Auth Sevice...');
+
+console.log(server, config)
+server.startServer(config)
--- AuthService/source/server/server.js
+++ AuthService/source/server/server.js
@@ -5,19 +5,19 @@ const api = require('../api/auth')
 
 const startServer = (options) => {
   return new Promise((resolve, reject) => {
-    if (!options.port) {
+    if (!options.serverSettings.port) {
       let err = new Error('The server should be started on available port')
       reject(err)
     }
 
     const app = express()
-    app.use(morgan('dev'))
+    app.use(logger('dev'))
     // app.use()
 
     api(app, options)
 
-    const server = app.listen(options.port, () = resolve(server))
+    const server = app.listen(options.serverSettings.port, () => resolve(server))
   })
-}
+};
 
-module.exports = Object.assign(({}, {startServer}));
+module.exports = Object.assign(({}, { startServer }));
