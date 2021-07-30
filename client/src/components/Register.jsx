ea7518cceab8dde443e03915c1d5de572bf137e4
rubber-bird <marko.turchyn@gmail.com>
2021-07-29 22:19:37 -0400

    Added Register Form

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,9 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  const handleSubmit = () => {
+    console.log('submit')
+  }
+
   return (
     <div>
       <h1>Register</h1>
+
+      <form>
+        <label>
+          Username:
+          <input
+            type='text'
+            onChange = {
+              (event) => setUsername(event.target.value)
+            }
+          ></input>
+        </label>
+
+        <label>
+          Email:
+          <input
+            type='email'
+            onChange = {
+              (event) => setUsername(event.target.value)
+            }
+          ></input>
+        </label>
+
+        <label>
+          Password:
+          <input
+            type='password'
+            onChange = {
+              (event) => setPassword(event.target.value)
+            }
+          ></input>
+        </label>
+
+        <label>
+          First Name:
+          <input
+            type='text'
+            onChange = {
+              (event) => setFirstName(event.target.value)
+            }
+          ></input>
+        </label>
+
+        <label>
+          Last Name:
+          <input
+            type='text'
+            onChange = {
+              (event) => setLastName(event.target.value)
+            }
+          ></input>
+        </label>
+
+        <input
+          type = 'button'
+          value = 'Submit'
+          onClick = {
+            (event) => handleSubmit()
+          }
+        ></input>
+      </form>
     </div>
   );
 };
