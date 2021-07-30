dce11898e4987aef575595ff9128331088edb238
rubber-bird <marko.turchyn@gmail.com>
2021-07-30 14:25:02 -0400

    Added cors middleware

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
 const AuthRouter = require('./routes/auth')
 
 app.use(logger('dev'));
+app.use(cors());
 
 app.use('/auth', AuthRouter);
 
