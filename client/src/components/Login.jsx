eb2555449c72af1e8a18166e32e001a0d46f7d7a
rubber-bird <marko.turchyn@gmail.com>
2021-07-29 22:09:39 -0400

    added login form

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  const handleSubmit = (event) => {
+    console.log('username', username, 'password', password)
+  }
+
   return (
     <div>
       <h1>Login</h1>
-      <p>dsds</p>
+
+      <form>
+        <label>Username:
+          <input
+            type='text'
+            name='name'
+            onChange={
+              (event) => setUsername(event.target.value)
+            }
+          ></input>
+        </label>
+
+        <label>Password:
+          <input
+            type='password'
+            name='password'
+            onChange={
+              (event) => setPassword(event.target.value)
+            }
+          ></input>
+        </label>
+
+        <input
+          type='button'
+          value='Submit'
+          onClick = {
+            (event) => handleSubmit(event)
+          }
+        ></input>
+      </form>
     </div>
   );
 };
