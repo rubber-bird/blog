3df57e4cacdb3fc0be49f55e562b36be70521b5b
rubber-bird <marko.turchyn@gmail.com>
2021-07-29 21:56:31 -0400

    added react router

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from './Home.jsx';
+import Login from './Login.jsx';
+import Register from './Register.jsx';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Main page</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
-};
+}
 
 export default App;
