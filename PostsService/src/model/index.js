4fa3e83f52242f15d6e9bed8d078b80098ad8a71
rubber-bird <marko.turchyn@gmail.com>
2021-07-31 15:53:24 -0400

    Added models

---
+++ PostsService/src/model/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const model = (dbConnection) => {
+  const getAllPosts = () => {
+    return new Promise(( resolve, reject ) => {
+      if()
+    })
+  }
+
+  return Object.create({
+    getAllPosts
+  })
+}
+
+const connect = (connection) => {
+  return new Promise((resolve, reject) => {
+    if (!connection) {
+      reject(new Error('no db connection'));
+    }
+    resolve(model(connection));
+  })
+}
+
+module.exports = Object.assign({}, { connect });
