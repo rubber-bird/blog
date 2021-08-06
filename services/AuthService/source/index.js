7ccfbf86e6dfcc323dc6a5aac2ab41c512e2859d
rubber-bird <marko.turchyn@gmail.com>
2021-08-06 19:04:51 -0400

    added a grpc client to auth service

--- services/AuthService/source/index.js
+++ services/AuthService/source/index.js
@@ -6,5 +6,26 @@ const config = require('./config/index');
 console.log('-----------------------');
 console.log('Starting Auth Sevice...');
 
-console.log(server, config);
-server.startServer(config);
+// console.log(server, config);
+// server.startServer(config);
+
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const PROTO_PATH = 'source/user.proto';
+const options = {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true
+};
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, options);
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+const user = protoDescriptor.user;
+
+const client = new user.UserService('0.0.0.0:50051', grpc.credentials.createInsecure());
+
+client.GetUserInformation({h1:'222'}, (err, res) => {
+  console.log(err,res)
+});
