55724e34cf71f949f3d5312f92d04d1de0dafa0e
rubber-bird <marko.turchyn@gmail.com>
2021-08-09 19:59:37 -0400

    Working version of login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,26 +1,55 @@
 import React, { useState } from 'react';
+import { gql, useMutation } from '@apollo/client';
 
 import login from '../api/auth/login.js';
 
+const LoginMutation = gql`
+  mutation login(
+    $username: String!
+    $password: String!
+    ) {
+    login(username: $username, password: $password) {
+      token
+    }
+  }
+`;
+
 const Login = () => {
+  const [loginFunc, {data, loading, error}] = useMutation(LoginMutation);
+
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSubmit = (event) => {
-    const requestObject = {
-      username: username,
-      password: password
-    };
+    event.preventDefault();
+    loginFunc({
+      variables: {
+        username: username,
+        password: password
+      }
+    })
+      .then(({data}) => {
+        console.log(data.login.token);
+      })
+    // const requestObject = {
+    //   username: username,
+    //   password: password
+    // };
 
-    login(requestObject)
-      .then((result) => {
-        console.log(result);
-      })
-      .catch((error) => {
-        console.error(error);
-      })
+    // login(requestObject)
+    //   .then((result) => {
+    //     console.log(result);
+    //   })
+    //   .catch((error) => {
+    //     console.error(error);
+    //   })
   }
 
+  if (loading) return <h2>Loading....</h2>
+  if (error) {
+    console.log(error)
+    return <h2>Error</h2>
+  }
   return (
     <div>
       <h1>Login</h1>
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
 
 import App from './components/App.jsx';
 
+const client = new ApolloClient({
+  uri: 'http://0.0.0.0:4000/graphql',
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root')
 );
